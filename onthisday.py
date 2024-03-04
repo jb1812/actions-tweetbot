6c310b03965dc40977299f3d0dd99f5083fea60a
Jinam Ravindra Bora <104253332+jb1812@users.noreply.github.com>
2024-03-04 09:36:04 +0530

    Update onthisday.py

--- onthisday.py
+++ onthisday.py
@@ -15,17 +15,12 @@ client = tweepy.Client(
 #tweet on this day facts
 
 date = datetime.datetime.now()
-print(date)
 api_url = 'https://api.api-ninjas.com/v1/historicalevents?month={}&day={}'.format(date.strftime("%m"), date.strftime("%d"))
-print(api_url)
 response = requests.get(api_url, headers={'X-Api-Key': "mEWcITGkxLzXHpcJo0bjPARdYumKoVDhdrpteRN3"})
 url_response = response.text
 a =  list(eval(url_response))
-print(a)
 i = 0
 while i <= len(a)-1:
-    print(i)
-    print(a[i])
     txt3 = 'Day: {} {}\n\nOn this day in the year {}, {}\n\n#OnThisDay #History'.format(date.strftime("%d"), date.strftime("%B"), a[i]['year'], a[i]['event'])
     client.create_tweet(text=txt3)
     i = i + 1
